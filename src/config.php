6e2e0c9db7bfbbb95c3c1648e4fd52ecde47abd1
Ayoosh Raj <rajayoosh03@gmail.com>
2025-04-18 13:20:44 +0530

    fixes 1

--- src/config.php
+++ src/config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    "api_key" => "AIzaSyDe_lWzivEo6E7d_7X7v4iTrwlHH_2ln4c",  // Replace with your actual API key
+    "api_key" => "api key",  // Replace with your actual API key
 ];
 ?>
